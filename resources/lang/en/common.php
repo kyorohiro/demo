62dc4d4b0e632b4edc0def9b574ff28381c69e58
kyorohiro <kyorohiro@gmail.com>
2018-12-05 23:23:06 +0000

    todo

---
+++ resources/lang/en/common.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'service_name' => 'ビシャス',
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,14 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ __('common.service_name') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
