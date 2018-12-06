322ce5b5ab88b0c131773162e0ab36568070dec5
kyorohiro <kyorohiro@gmail.com>
2018-12-06 19:22:19 +0000

    mod home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,26 @@
 @section('content')
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <?php
+        $name = $user->name;
+        $email = $user->email;
+        $email[1] = "*";
+        $email[2] = "*";
+    ?>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    {{ __('auth.throttle')}}
-                    You are logged in!
+   <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">Profile</div>
+                    <div class="card-body">
+                        {{$name}} <br>
+                        {{$email}}<br>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 </div>
 @endsection
