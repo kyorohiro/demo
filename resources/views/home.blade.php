a642db626ad09038574ade9c76caaefd24afca26
kyorohiro <kyorohiro@gmail.com>
2018-12-06 19:37:32 +0000

    mod home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,26 @@
             </div>
         </div>
     </div>
+    <br>
+    <br>
+    <div class="container">
+        <div class="list-group">
+          <div class="list-group-item active">契約一覧</div>
+          <a href="#" class="list-group-item">xxxxxxの授業</a>
+          <a href="#" class="list-group-item">yyyの授業</a>
+        </div>
+    </div>
+
+
+    <br>
+    <br>
+    <div class="container">
+        <div class="list-group">
+          <div class="list-group-item active">相談から契約まで</div>
+          <a href="#" class="list-group-item">mmmの授業</a>
+          <a href="#" class="list-group-item">nnnの授業</a>
+        </div>
+    </div>
 
 </div>
 @endsection
