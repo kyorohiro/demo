cf85848914c328122a91043a9e44ad011f6467a1
kyorohiro <kyorohiro@gmail.com>
2018-12-07 00:06:49 +0000

    add relations user and userinfo

--- app/Userinfo.php
+++ app/Userinfo.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Userinfo extends Model
 {
     //
+    protected $fillable = [
+        "user_id","firstname","lastname",
+        "zip_code","country_code","lang_code",
+        "prefucture","address","tel","profile"
+    ];
+    
+    public function user() {
+        return $this->belongsTo("App\User","user_id","id");
+    }
 }
+
